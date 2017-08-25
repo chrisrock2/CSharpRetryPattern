2f421fb7fbb06557deb3a6dc6661f88dbb9cef5d
chrisrock2 <chrisrock2@gmail.com>
2017-08-25 11:41:02 -0400

    Initial checkin

---
+++ Retry.cs
@@ -0,0 +1,68 @@
+﻿/* 
+
+Code by Chris Rock (chrisrock2@gmail.com)
+a retry pattern for c# that uses Func<T> or Action
+
+// to improve, you can catch a better exception 
+ */
+
+using System;
+using System.Globalization;
+
+namespace RetryPattern
+{
+    public static class Retry
+    {
+        public static T Do<T>(int numberOfRetries, Func<T> operation, int pauseBetweenExecutions = 500)
+        {
+            var currentTries = 0;
+
+            do
+            {
+                try
+                {
+                    currentTries++;
+                    return operation();
+                }
+                catch (Exception)
+                {
+                    if (currentTries == numberOfRetries)
+                        throw;
+
+                    System.Threading.Thread.Sleep(pauseBetweenExecutions);
+                }
+            } while (currentTries < numberOfRetries);
+
+            throw new RetryException(string.Format(CultureInfo.InvariantCulture, "Retried operation '{0}' times without an exception", numberOfRetries));
+        }
+
+        public static void Do(int numberOfRetries, Action operation, int pauseBetweenExecutions = 500)
+        {
+            var currentTries = 0;
+
+            do
+            {
+                try
+                {
+                    currentTries++;
+                    operation();
+                    return;
+                }
+                catch (Exception)
+                {
+                    if (currentTries == numberOfRetries)
+                        throw;
+
+                    System.Threading.Thread.Sleep(pauseBetweenExecutions);
+                }
+            } while (currentTries < numberOfRetries);
+
+            throw new RetryException(string.Format(CultureInfo.InvariantCulture, "Retried operation '{0}' times without an exception", numberOfRetries));
+        }
+    }
+
+    public class RetryException : Exception
+    {
+        public RetryException(string message) : base(message) { }
+    }
+}
---
+++ RetryPattern.Tests/RetryTests.cs
@@ -0,0 +1,62 @@
+﻿/*
+
+Code by Chris Rock (chrisrock2@gmail.com)
+a retry pattern for c# that uses Func<T> or Action
+
+*/
+using System.IO;
+using NUnit.Framework;
+
+namespace RetryPattern.Tests
+{
+    [TestFixture]
+    public class RetryTests
+    {
+        // these tests are conclusive
+        // there are more to show you how to use the retry pattern
+        [Test]
+        public void TestRetryGetName()
+        {
+            var name = Retry.Do(3, () => GetName());
+            Assert.That(name, Is.EqualTo("Chris"));
+        }
+
+        [Test]
+        public void TestRetryFunctionWithException()
+        {
+            Assert.Throws<FileNotFoundException>(() => Retry.Do(3, ExecuteWithException));
+        }
+        
+        [Test]
+        public void TestReturnAction()
+        {
+            Assert.DoesNotThrow(() => Retry.Do(3, Execute));
+        }
+        
+        [Test]
+        public void TestGetNameWithException()
+        {
+            Assert.Throws<FileNotFoundException>(() => Retry.Do(3, () => GetNameWithException()));
+        }
+
+        public void Execute()
+        {
+            // do something
+        }
+
+        public void ExecuteWithException()
+        {
+            throw new FileNotFoundException();
+        }
+        
+        public string GetName()
+        {
+            return "Chris";
+        }
+
+        public string GetNameWithException()
+        {
+            throw new FileNotFoundException();
+        }
+    }
+}
